d314bbe5a6e1d8515cecede95ae60e0dd8d81569
Puja <kripuja1999@gmail.com>
2023-06-17 08:17:15 +0530

    Uploaded all files

---
+++ app/src/main/java/com/puja/practice/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.puja.practice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_relative_layout)
+    }
+}
